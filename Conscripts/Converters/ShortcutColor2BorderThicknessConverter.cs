a83ba8de90a6f34f487bf90a11d2a5f6c84d7acd
姚一鸣 <yaoyiming123@live.com>
2025-04-16 23:57:12 +0800

    增加对卡片的边框处理

---
+++ Conscripts/Converters/ShortcutColor2BorderThicknessConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Conscripts.ViewModels;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml;
+using Conscripts.Models;
+
+namespace Conscripts.Converters
+{
+    internal class ShortcutColor2BorderThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                return value?.ToString() == ShortcutColorEnum.Transparent.ToString() ? new Thickness(1) : new Thickness(0);
+            }
+            catch (Exception ex) { System.Diagnostics.Trace.WriteLine(ex); }
+            return new Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
